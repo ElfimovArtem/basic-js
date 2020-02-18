a79a638b47e14088b8b82c93f1efbff725d268ad
ElfimovArtem <elfimov021@yandex.ru>
2020-02-18 17:56:33 +0300

    feat: The solution to the what-season assignment.

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,18 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date instanceof Date && Object.getOwnPropertyNames(date).length === 0) {
+    const month = date.getMonth();
+    if (month === 1 || month === 0 || month === 11) {
+      return 'winter';
+    } else if (month > 1 && month < 5) {
+      return 'spring';
+    } else if (month > 4 && month < 8) {
+      return 'summer';
+    } else {
+      return 'autumn';
+    }
+  } else if(!date) {
+    return 'Unable to determine the time of year!';
+  } else {
+    throw new Error;
+  }
 };
