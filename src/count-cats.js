20361528bd5d7c39d49e1e8995d15bd44babaf87
ElfimovArtem <elfimov021@yandex.ru>
2020-02-18 14:33:47 +0300

    feat: The solution to the count-cats assignment.

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,3 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  return matrix.reduce((acc, cV) => acc + cV.reduce((sum, n) => sum + ((n === "^^") ? 1 : 0), 0), 0);
 };
